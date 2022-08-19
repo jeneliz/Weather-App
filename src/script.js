a676897a00900cbe33c1792a9d85daf665c4e919
jeneliz <109545820+jeneliz@users.noreply.github.com>
2022-08-18 21:56:27 -0700

    Saved work from CodeSandbox

---
+++ src/script.js
@@ -0,0 +1,67 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+
+let h5 = document.querySelector("h5");
+h5.innerHTML = `${day} ${now.toLocaleTimeString()}`;
+//
+/* function city(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#local-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = searchInput.value;
+} 
+
+let localForm = document.querySelector("#local-form");
+localForm.addEventListener("submit", city); 
+
+*/
+
+// Resubmit
+/*
+#temp (Current Temp)
+#local-form (Form Id)
+#local-input (text box id)
+*/
+
+function showCurrentWeather(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function citySearch(event) {
+  event.preventDefault();
+  let apiKey = `5ee080a084160ec534f65b526b1fa3f4`;
+  let city = document.querySelector("#local-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showCurrentWeather);
+}
+
+function convertFahrenheit(event) {
+  event.preventDefault();
+  let tempDisplay = document.querySelector("#temp");
+  tempDisplay.innerHTML = "42°";
+}
+
+function convertCelsius(event) {
+  event.preventDefault();
+  let tempDisplay = document.querySelector("#temp");
+  tempDisplay.innerHTML = `108°`;
+}
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertFahrenheit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertCelsius);
